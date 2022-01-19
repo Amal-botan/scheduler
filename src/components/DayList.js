1556ddefaadc922250d44438c06f7c90fcda3f5f
Amal <amalbotan254@gmail.com>
2022-01-19 11:21:34 -0500

    Built DayList

---
+++ src/components/DayList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+import DayListItem from "components/DayListItem";
+
+
+export default function DayList(props) {
+  const days = props.days;
+
+  const dayComponents = days.map(dayOfWeek => <DayListItem key={dayOfWeek.id}
+    name={dayOfWeek.name} 
+    spots={dayOfWeek.spots} 
+    selected={dayOfWeek.name === props.day}
+    setDay={props.setDay}  /> 
+    )
+  
+
+
+ 
+
+  return (
+    <ul>{dayComponents}</ul>
+  );
+}
