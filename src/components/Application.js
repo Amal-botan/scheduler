4a4a83205b8aadb151211f5955867bad377c5a4e
Amal <amalbotan254@gmail.com>
2022-01-20 19:55:12 -0500

    Managed and refractored the state

--- src/components/Application.js
+++ src/components/Application.js
@@ -44,9 +44,22 @@ const appointments = [
   }
 ];
 
+
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   const appointmentList = appointments.map((appointment) => {
     return (
@@ -62,7 +75,7 @@ export default function Application(props) {
     });
   },[]);
 
-  console.log("day of Daylist",day);
+  console.log("day of Daylist",state.day);
 
   return (
     <main className="layout">
@@ -75,8 +88,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
         <DayList
-          days={days}
-          value={day}
+          days={state.days}
+          value={state.day}
           onChange={setDay}
         />
         </nav>
